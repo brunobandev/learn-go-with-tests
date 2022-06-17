269221d42b790affd469c836505b3fc9ad8ecacb
Bruno <bruno@pop-os.localdomain>
2022-06-17 20:28:35 +0100

    first commit

---
+++ 001-hello-world/hello.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+const spanish = "Spanish"
+const french = "French"
+const portuguese = "Portuguese"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const portugueseHelloPrefix = "Oi, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case portuguese:
+		prefix = portugueseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
