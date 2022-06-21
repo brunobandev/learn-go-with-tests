0a1577252b5d413f16358a422e6fb6131b261fd4
Bruno Bandeira <brunoban.dev@gmail.com>
2022-06-21 20:21:46 +0100

    testing map cruds

--- 007-maps/dictionary.go
+++ 007-maps/dictionary.go
@@ -2,6 +2,58 @@ package main
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+const (
+	ErrNotFound         = DictionatyErr("could not find the word you were looking for")
+	ErrWordExist        = DictionatyErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionatyErr("cannot update word because it does not exist")
+)
+
+type DictionatyErr string
+
+func (e DictionatyErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExist
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
